c04bf80b6a4c43d9cdfb0fb8542016647adccdee
alberto.borguesani <alberto.borguesani@infracommerce.com.br>
2017-09-25 09:15:16 -0300

    Aula 04 Criando nosso ListModel

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -2,35 +2,47 @@ class NegociacaoController {
 
     constructor(){
 
+        /*Criamos uma negociação */
         let $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event) {
-        
-        event.preventDefault(); 
 
-        let helper = new DateHelper(); 
-        let helper2 = new DateHelper(); 
-        let negociacao = new Negociacao(
+        event.preventDefault();
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+
+    }
+
+    _criaNegociacao(){
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
-            this._inputValor.value
-        ); 
+            this._inputValor.value);
+    }
 
-        console.log(DateHelper.dataParaTexto(negociacao.data));        
+
+    /*Só pode ser chamado no formulario */
+    _limpaFormulario(){
+        this._inputData = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+
+        this._inputData.focus();
     }
 }
 
 
 /**
- * Temos a string que esperávamos receber. Mas, além de usar o split(), 
- * poderíamos utilizar o replace(). Adicionaremos uma expressão regular 
- * pedindo que seja trocado o hífen de todas as ocorrências da string 
+ * Temos a string que esperávamos receber. Mas, além de usar o split(),
+ * poderíamos utilizar o replace(). Adicionaremos uma expressão regular
+ * pedindo que seja trocado o hífen de todas as ocorrências da string
  * (ou seja, global) por ,: replace(/-/g, ',').
- * 
+ *
    adiciona(event) {
 
     event.preventDefault();
