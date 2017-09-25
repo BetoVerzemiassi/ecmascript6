11c20eb0b2a297a6da4c13a95fd099dbe67df2b9
alberto.borguesani <alberto.borguesani@infracommerce.com.br>
2017-09-25 09:48:34 -0300

    Aula 04 Ajuste na expressao regular

--- aluraframe/client/js/app/helpers/DateHelper.js
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -3,16 +3,16 @@ class DateHelper {
     constructor(){
         throw new Error('Esta classe não pode ser instanciada');
     }
-    
+
     static dataParaTexto(data){
-        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;//Template String 
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;//Template String
     }
 
     static textoParaData(texto){
 
-        if(!/\d{4}-\d{2}-\d{2}/.test(texto)) 
+        if(!/^\d{4}-\d{2}-\d{2}$/.test(texto))//^ indica "começando com" e $ "terminando com"
             throw new Error("Deve estar no formato aaaa-mm-dd");
-            
+
         return new Date(...texto.split('-').map((item,indice) => item - indice % 2));//... = spread
-    }    
+    }
 }
